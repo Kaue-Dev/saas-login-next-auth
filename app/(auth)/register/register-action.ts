c94d962d683e50316a2b0b0b6aaeeb38acdf4407
Kaue-Dev <kaueandradepadilha@gmail.com>
2025-07-18 16:40:20 -0300

    feat: implements better error message

--- app/(auth)/register/register-action.ts
+++ app/(auth)/register/register-action.ts
@@ -3,7 +3,7 @@
 import db from "@/lib/db";
 import { hashSync } from "bcrypt-ts";
 
-export default async function registerAction(formData: FormData) {
+export default async function registerAction(_prevState: any, formData: FormData) {
   const entries = Array.from(formData.entries());
   const data = Object.fromEntries(entries) as {
     name: string;
@@ -13,16 +13,25 @@ export default async function registerAction(formData: FormData) {
   };
   
   if (!data.name || !data.email || !data.password || !data.confirmPassword) {
-    throw new Error("All fields are required.");
+    return {
+      message: "All fields are required.",
+      success: false,
+    }
   }
 
   if (data.password !== data.confirmPassword) {
-    throw new Error("Passwords do not match.");
+    return {
+      message: "Passwords do not match.",
+      success: false,
+    }
   }
 
   const userAlreadyExists = await db.user.findUnique({ where: { email: data.email } })
   if (userAlreadyExists) {
-    throw new Error("User already exists.");
+    return {
+      message: "User already exists.",
+      success: false,
+    }
   }
 
   await db.user.create({
@@ -32,4 +41,9 @@ export default async function registerAction(formData: FormData) {
       password: hashSync(data.password), 
     }
   })
+
+  return {
+    message: "User registered successfully.",
+    success: true,
+  }
 }
