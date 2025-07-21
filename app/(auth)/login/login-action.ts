72e0130cd4ceed2135d5a9f5c053ba0a16201bab
Kaue-Dev <kaueandradepadilha@gmail.com>
2025-07-21 16:55:24 -0300

    feat: implements redirection to home after login

--- app/(auth)/login/login-action.ts
+++ app/(auth)/login/login-action.ts
@@ -1,29 +1,37 @@
 "use server";
 
 import { signIn } from "@/auth";
+import { isRedirectError } from "next/dist/client/components/redirect-error";
 
 export default async function loginAction(_prevState: any, formData: FormData) {
   try {
     await signIn('credentials', {
       email: formData.get("email"),
       password: formData.get("password"),
-      redirect: false,
+      redirect: true,
+      redirectTo: "/",
     })
+
     return {
       message: "Login successful.",
       success: true
     }
   } catch (error: any) {
+    // Handle redirect errors specifically (community solution)
+    if (isRedirectError(error)) {
+      throw error;
+    }
+
     if (error.type === "CredentialsSignin") {
       return {
         message: "Invalid email or password.",
         success: false,
       }
-    } else {
-      return {
-        message: "An unexpected error occurred. Please try again later.",
-        success: false,
-      }
+    }
+
+    return {
+      message: "An unexpected error occurred. Please try again later.",
+      success: false,
     }
   }
 }
